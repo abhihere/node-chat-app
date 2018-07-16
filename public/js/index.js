a8e1645499364f02a31b8c92d7c82e9a9d7f1768
abhihere <abhijeetraut58@gmail.com>
2018-07-16 14:20:53 +0530

    add style to chat app

--- public/js/index.js
+++ public/js/index.js
@@ -35,7 +35,7 @@ jQuery('#message-form').on('submit', function(e){
     from: 'User',
     text: jQuery('[name=message]').val()
   }, function(){
-
+    jQuery('[name=message]').val('');
   })
 })
 
@@ -44,13 +44,16 @@ locationButton.on('click', function(){
   if(!navigator.geolocation){
     return alert('Geolocation not supported by your browser');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending...');
 
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function(){
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('unable to fetch location');
   })
 });
